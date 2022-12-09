409ebbc24da14aec72ec05013f7760618556eeb5
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2022-12-08 16:09:25 -0800

    final commit

--- refactor/dpk.js
+++ refactor/dpk.js
@@ -3,26 +3,29 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = TRIVIAL_PARTITION_KEY;
 
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+    candidate = event.partitionKey
+      ? event.partitionKey
+      : crypto
+          .createHash("sha3-512")
+          .update(JSON.stringify(event))
+          .digest("hex");
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
+
   return candidate;
 };
+
+// The original function had multiple superfluous nested condition checks which i simplified with concise ternaries.
+// In the refactored code it is easier to see that we are simply reassigning a value to the "candidate" variable
+// only if there is an event passed to the function, and after we have dealth with the passed in parameter we do checks
+// on the type of value we have and its length and take apporpriate action
